c54d0c459cb75e1baf7b78fb03d2d9980604fc7e
Farid Kurniawan <farid.kurniawan0412@gmail.com>
2026-02-10 11:36:18 +0700

    membuat model untuk Category, Product, StockIn, dan StockOut

---
+++ app/Models/StockOut.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StockOut extends Model
+{
+    protected $fillable = ["product_id", "qty", "description"];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
