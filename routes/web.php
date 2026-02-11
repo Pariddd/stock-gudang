bbdb0bb2d97a09c061c5bf7b97fbeec97f32345d
Farid Kurniawan <farid.kurniawan0412@gmail.com>
2026-02-11 16:32:24 +0700

    refactor routing structure for dashboard and related resources

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StockInController;
@@ -9,22 +10,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])
+        ->name('index');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->name('dashboard');
+    Route::get('products/fetch', [ProductController::class, 'fetch'])
+        ->name('products.fetch');
+    Route::resource('products', ProductController::class);
 
-Route::resource('dashboard/products', ProductController::class);
-Route::get('/products/fetch', [ProductController::class, 'fetch'])->name('products.fetch');
+    Route::resource('categories', CategoryController::class);
 
-Route::get('dashboard/stock-in', [StockInController::class, 'index'])
-    ->name('stock-in.index');
+    Route::get('stock-in', [StockInController::class, 'index'])
+        ->name('stock-in.index');
 
-Route::post('/stock-in', [StockInController::class, 'store'])
-    ->name('stock-in.store');
+    Route::post('stock-in', [StockInController::class, 'store'])
+        ->name('stock-in.store');
 
-Route::get('dashboard/stock-out', [StockOutController::class, 'index'])
-    ->name('stock-out.index');
+    Route::get('stock-out', [StockOutController::class, 'index'])
+        ->name('stock-out.index');
 
-
-Route::post('/stock-out', [StockOutController::class, 'store'])
-    ->name('stock-out.store');
+    Route::post('stock-out', [StockOutController::class, 'store'])
+        ->name('stock-out.store');
+});
